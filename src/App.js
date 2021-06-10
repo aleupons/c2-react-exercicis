2c82547511dd908e4dec0c6738ddb33e349e0d27
aleupons <apahoquei@gmail.com>
2021-06-10 16:50:44 +0200

    Funcions clicar, canviar estat, maquetació

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import { Cercle } from "./components/Cercle";
-import { useState } from "react";
+
 function App() {
   const numeroCercles = 5;
   const cercles = new Array(numeroCercles);
-  const [estat, setEstat] = useState(false);
-  const modificaEstat = () => {
-    setEstat(true);
-  };
+  cercles.fill("", "");
+
   return (
-    <ul>
-      {cercles.map((cercle, i) => (
-        <Cercle
-          className="cercle"
-          estat={estat}
-          onClick={modificaEstat}
-          key={i}
-        />
-      ))}
-    </ul>
+    <>
+      <ul>
+        {cercles.map((cercle) => (
+          <Cercle key={cercle} />
+        ))}
+      </ul>
+    </>
   );
 }
 
--- src/components/Cercle.js
+++ src/components/Cercle.js
@@ -1,4 +1,16 @@
-export const Cercle = (props) => {
-  const { estat, modificaEstat } = props;
-  return <li></li>;
+import { useState } from "react";
+
+export const Cercle = () => {
+  const [estat, setEstat] = useState(false);
+  const modificaEstat = () => {
+    setEstat(estat ? false : true);
+  };
+  const classe = estat ? "cercle taronja" : "cercle";
+  return (
+    <li
+      className="cercle"
+      onClick={modificaEstat}
+      style={{ backgroundColor: estat ? "orange" : "lightgrey" }}
+    ></li>
+  );
 };
